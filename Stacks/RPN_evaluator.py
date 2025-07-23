260297263db644522d5321c391b22e6dbb17ac8f
Rudra280906 <rudra280906@gmail.com>
2025-07-22 19:32:28 -0700

    Completed RPN Evaluator Problem

--- Stacks/RPN_evaluator.py
+++ Stacks/RPN_evaluator.py
@@ -1,3 +1,16 @@
-# In progress, still trying to understand how to do the problem
+# Understood RPN Evaluator Problem and Solution
 def RPN_evaluator(expression):
-    pass
+    expression = expression.split(", ")
+    intermediate_results = []
+    OPERATORS = {"+": lambda y, x: int(x) + int(y), "-": lambda y, x: int(x) - int(y), "*": lambda y, x: int(x) * int(y), "/": lambda y, x: int(int(x) / int(y))}
+    for exp_ in expression:
+        if exp_ in OPERATORS:
+            y = intermediate_results.pop()
+            x = intermediate_results.pop()
+            intermediate_results.append(OPERATORS[exp_](y, x))
+        else:
+            intermediate_results.append(exp_)
+    return intermediate_results[-1]
+
+
+print(RPN_evaluator("3, 4, +"))
