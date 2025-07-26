7b314349778d3bbae0ddbd42263690d618e1070c
Rudra280906 <rudra280906@gmail.com>
2025-07-25 20:14:02 -0700

    Fixed FirstUniqChar Queue

--- Queues/First Unique Character of a String.py
+++ Queues/First Unique Character of a String.py
@@ -1,13 +1,21 @@
 # Find the first non-repeating character and return its index else -1
 def firstUniqChar(s):
     queue = []
+    queue2 = []
     for i in s:
         if len(queue) == 0:
             queue.append(i)
+            continue
         elif i == queue[0]:
             queue.pop(0)
+            queue2.append(i)
         else:
             queue.append(i)
+    if len(queue) == 0:
+        return -1
+    for i in queue2:
+        if i in queue:
+            queue.remove(i)
     if len(queue) == 0:
         return -1
     else:
@@ -27,5 +35,7 @@ def firstUniqCharHash(s):
             return i
     return -1
 
-print(firstUniqChar("aadadaad"))
-print(firstUniqCharHash("aadadaad"))
+print(firstUniqChar("aadaadad"))
+print(firstUniqCharHash("aadaadad"))
+print(firstUniqChar("aaad"))
+print(firstUniqCharHash("aaad"))
